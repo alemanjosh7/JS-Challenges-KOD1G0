bb59aa46dfd8511dd5596baaaf3a260c49bfae8e
alemanjosh7 <josuealeman2003@gmail.com>
2021-03-12 17:38:08 -0600

    Finishing the index page and creating a modal to visualize the credit card

--- 02-Validacion/tailwind.config.js
+++ 02-Validacion/tailwind.config.js
@@ -2,10 +2,14 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    fontFamily: {
+      codepro: ["Mulish"],
+      lato: ["Lato"],
+    },
     extend: {},
   },
   variants: {
     extend: {},
   },
   plugins: [],
-}
+};
