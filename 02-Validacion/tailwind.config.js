c054978d30a3ffb139fe9eafcb83c1394090be0f
alemanjosh7 <josuealeman2003@gmail.com>
2021-03-12 20:05:50 -0600

    Advance of the day: I make the front of the credit card, styles, etc

--- 02-Validacion/tailwind.config.js
+++ 02-Validacion/tailwind.config.js
@@ -2,6 +2,9 @@ module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
   theme: {
+    minHeight: {
+      extra: "315px",
+    },
     fontFamily: {
       codepro: ["Mulish"],
       lato: ["Lato"],
